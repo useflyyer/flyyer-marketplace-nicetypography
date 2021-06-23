09a7d50bc5dfe2f4253e2f001b29869ec087a2de
Patricio López Juri <lopezjuripatricio@gmail.com>
2021-06-23 09:13:51 -0400

    feat: Prepare public release

--- flayyer.config.js
+++ flayyer.config.js
@@ -1,6 +1,7 @@
 // Created with create-flayyer-app@1.17.0
 
 const {config} = require('@flayyer/flayyer-types');
+const { default: endent } = require("endent");
 require('dotenv').config();
 
 module.exports = config({
@@ -9,6 +10,16 @@ module.exports = config({
   deck: 'nice-typography',
 
   // Optionals
-  name: 'Nice-Typography',
-  description: 'Created with create-flayyer-app'
+  name: 'Nice Typography',
+  description: endent`
+    Template for every size to display a centered text with any Google Font typography.
+
+    Optionally supports a background image and custom colors.
+  `,
+  homepage: "https://flayyer.com",
+  keywords: ["flayyer", "typography", "tailwind", "tailwindcss"],
+  license: "MIT",
+  private: false, // Make it public at flayyer.com/community
+  repository: "https://github.com/flayyer/flayyer-marketplace-nicetypography",
+  sizes: ["THUMBNAIL" | "BANNER" | "SQUARE" | "STORY" ],
 });
